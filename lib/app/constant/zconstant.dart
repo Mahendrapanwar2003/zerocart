d65adfd84b6275c6cc97c37ac97a911a5afc54ea
dollo <dollopmanish@gmail.com>
2023-08-24 09:26:22 +0530

    first 1.0

--- lib/app/constant/zconstant.dart
+++ lib/app/constant/zconstant.dart
@@ -18,18 +18,4 @@ class Zconstant{
   static String textSomethingWentWrongTitle = "Ohh.. No something went wrong";
   static String textSomethingWentWrongDis = "We are working on fixing the problem. Please refresh the page and try again.";
 
-
-
-  static String imageSomethingWentWrong = "assets/images/something_went_wrong.png";
-  static String imageNoDataFound = "assets/images/no_data_found.png";
-  static String imageNoInternetConnection = "assets/images/no_internet_image.png";
-
-  static String textNoInternetTitle = "Oops....";
-  static String textNoInternetDis = "There is a connection error. Please check your internet and try again.";
-  static String textNoDataTitle = "Oops.... No result found.";
-  static String textNoDataDis = "We couldn’t found what you searched for.Try searching again.";
-  static String textSomethingWentWrongTitle = "Ohh.. No something went wrong";
-  static String textSomethingWentWrongDis = "We are working on fixing the problem. Please refresh the page and try again.";
-
-
 }
